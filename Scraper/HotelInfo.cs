6c90587b843aa9695aa07fd5057fd7d5e9cfce59
Karl Park <krlsib@g.ucla.edu>
2018-08-10 16:10:46 -0700

    first commit

---
+++ Scraper/HotelInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    class HotelInfo
+    {
+        public string HotelName { get; set; }
+        public string HotelStatus { get; set; }
+    }
+}
--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using AngleSharp.Parser.Html;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Scraper
@@ -10,6 +14,42 @@ namespace Scraper
     {
         static void Main(string[] args)
         {
+            using (var client = new HttpClient())
+            {
+                var html = client.GetStringAsync("https://en.wikipedia.org/wiki/List_of_hotels_in_the_United_States").Result;
+
+                var parser = new HtmlParser();
+
+                var document = parser.Parse(html);
+
+                var losAngeles = document.QuerySelector("#Los_Angeles_County");
+                var divToSearch = losAngeles.ParentElement.NextElementSibling;
+
+                var tableRows =
+                    divToSearch.QuerySelectorAll("li");
+
+                List<HotelInfo> results = new List<HotelInfo>();
+
+                foreach (var li in tableRows)
+                {
+                    var hotelInfo = new HotelInfo();
+
+
+                    var aElement = li.QuerySelector("a");
+                    hotelInfo.HotelName = aElement.TextContent;
+
+                    var secondElement = aElement.NextSibling;
+
+                    if (secondElement != null)
+                    {
+                        hotelInfo.HotelStatus = secondElement.TextContent;
+                    }
+
+                    results.Add(hotelInfo);
+                }
+
+                Console.WriteLine(JsonConvert.SerializeObject(results, Formatting.Indented));
+            }
         }
     }
 }
